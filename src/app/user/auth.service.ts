3fc7b3866be5714d95dae79b615c21607deb3cda
Daniyal <daniyal.moeen@greenlightlabs.tech>
2023-01-26 14:48:07 +0500

    Package Issues has been fixed

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { BehaviorSubject, Subject, Observable } from 'rxjs';
+import {CognitoUser, CognitoUserAttribute, CognitoUserPool} from "amazon-cognito-identity-js";
 
 import { User } from './user.model';
+
+const POOL_DATA = {
+  UserPoolId:'eu-west-1_9YePsbWBN',
+  ClientId:'191hkn4vl2s1b54brhj4j5k8lg'
+};
+const userPool= new CognitoUserPool(POOL_DATA);
 
 @Injectable()
 export class AuthService {
   authIsLoading = new BehaviorSubject<boolean>(false);
   authDidFail = new BehaviorSubject<boolean>(false);
   authStatusChanged = new Subject<boolean>();
+  registeredUser:CognitoUser|any;
   constructor(private router: Router) {}
   signUp(username: string, email: string, password: string): void {
     this.authIsLoading.next(true);
@@ -17,10 +25,22 @@ export class AuthService {
       email: email,
       password: password
     };
+    const attrList:CognitoUserAttribute[]=[];
     const emailAttribute = {
       Name: 'email',
       Value: user.email
     };
+    attrList.push(new CognitoUserAttribute(emailAttribute));
+    userPool.signUp(user.username,user.password,attrList,[],(err,result)=>{
+      if(err){
+        this.authDidFail.next(true);
+        this.authIsLoading.next(false);
+        return;
+      }
+      this.authDidFail.next(false);
+      this.authIsLoading.next(false);
+      this.registeredUser=result?.user;
+    })
     return;
   }
   confirmUser(username: string, code: string) {
