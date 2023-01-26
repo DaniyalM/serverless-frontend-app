c8c74356390fc812923cb996b0086e7f66b36a32
Daniyal <daniyal.moeen@greenlightlabs.tech>
2023-01-26 14:07:32 +0500

    ServerlessFixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './user/signup/signup.component';
+import { SigninComponent } from './user/signin/signin.component';
+import { CompareComponent } from './compare/compare.component';
+import { AuthService } from './user/auth.service';
+import { CompareService } from './compare/compare.service';
+import { CompareInputComponent } from './compare/compare-input/compare-input.component';
+import { CompareResultsComponent } from './compare/compare-results/compare-results.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent  ,
+      SignupComponent,
+    SigninComponent,
+    CompareComponent,
+    CompareInputComponent,
+    CompareResultsComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [AuthService, CompareService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/user/auth.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject, Subject, Observable } from 'rxjs';
+
+import { User } from './user.model';
+
+@Injectable()
+export class AuthService {
+  authIsLoading = new BehaviorSubject<boolean>(false);
+  authDidFail = new BehaviorSubject<boolean>(false);
+  authStatusChanged = new Subject<boolean>();
+  constructor(private router: Router) {}
+  signUp(username: string, email: string, password: string): void {
+    this.authIsLoading.next(true);
+    const user: User = {
+      username: username,
+      email: email,
+      password: password
+    };
+    const emailAttribute = {
+      Name: 'email',
+      Value: user.email
+    };
+    return;
+  }
+  confirmUser(username: string, code: string) {
+    this.authIsLoading.next(true);
+    const userData = {
+      Username: username,
+    };
+  }
+  signIn(username: string, password: string): void {
+    this.authIsLoading.next(true);
+    const authData = {
+      Username: username,
+      Password: password
+    };
+    this.authStatusChanged.next(true);
+    return;
+  }
+  getAuthenticatedUser() {
+  }
+  logout() {
+    this.authStatusChanged.next(false);
+  }
+  isAuthenticated(): Observable<boolean> {
+    const user:any = this.getAuthenticatedUser();
+    const obs = Observable.create((observer:any) => {
+      if (!user) {
+        observer.next(false);
+      } else {
+        observer.next(false);
+      }
+      observer.complete();
+    });
+    return obs;
+  }
+  initAuth() {
+    this.isAuthenticated().subscribe(
+      (auth) => this.authStatusChanged.next(auth)
+    );
+  }
+}
